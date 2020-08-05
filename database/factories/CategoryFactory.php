f34b426b70a2e26b67def09ae04a7559589338e3
Daniel Ehrenhofer <info@danielehrenhofer.de>
2020-08-05 09:32:10 +0200

    Update CategoryFactory.php

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Faker\Generator as Faker;
 
 $factory->define(Rinvex\Categories\Models\Category::class, function (Faker $faker) {
-    return ['name' => [$faker->languageCode => $faker->title], 'slug' => $faker->slug];
+    return ['name' => [$faker->languageCode => $faker->title], 'slug' => [$faker->languageCode => $faker->slug]];
 });
