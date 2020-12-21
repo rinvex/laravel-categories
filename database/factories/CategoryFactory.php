1d5ea0c07894813b719a9d31df93d9e072dd9efd
Abdelrahman Omran <me@omranic.com>
2020-12-22 02:51:57 +0400

    Upgrade to Laravel v8

--- database/factories/CategoryFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Faker\Generator as Faker;
-
-$factory->define(Rinvex\Categories\Models\Category::class, function (Faker $faker) {
-    return ['name' => [$faker->languageCode => $faker->title], 'slug' => $faker->slug];
-});
